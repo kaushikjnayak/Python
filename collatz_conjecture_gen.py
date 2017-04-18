044cbfa6525e633df8f1d9c3acdc264c139374be
kaushikjnayak <kaushikjnayak@gmail.com>
2017-04-18 15:07:51 +0530

    Create collatz_conjecture_gen.py
    apply the Collatz conjecture to first n natural numbers and print the end result.
    
    https://en.wikipedia.org/wiki/Collatz_conjecture

---
+++ collatz_conjecture_gen.py
@@ -0,0 +1,17 @@
+def collatzgen(num):
+  if type(num) == int:
+    
+    for i in range(1,num):
+      iter = 0
+      r = i
+      while ( r != 1):
+          if r % 2 == 0:
+             r =  int(r/2)
+          else: 
+             r = 3*r + 1
+          iter += 1
+      print   ( "Number - {} ,final = {} ,Total iterations - {}".format(i,r,iter))    
+  else:
+     print ( "{} is non-numeric".format(num))
+
+collatzgen(10000)
