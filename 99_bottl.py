e6f2c318513875b69985265916f5a76f4861435a
Kaushik Nayak <kaushikjnayak@gmail.com>
2017-04-28 23:10:23 +0530

    lyrics
    The lyrics to the song 99 Bottles of Beer

--- 99_bottl.py
+++ 99_bottl.py
@@ -1,3 +1,4 @@
+#The lyrics to the song 99 Bottles of Beer
 String_orig = '''{0} bottles of beer on the wall, {0} bottles of beer.
 Take one down, pass it around, {1} bottles of beer on the wall'''
 
