ee0cfaaddec4539ac7eeac36f2de5bed58c5af19
Kaushik Nayak <kaushikjnayak@gmail.com>
2017-05-19 12:42:21 +0530

    Update dates.py

--- dates.py
+++ dates.py
@@ -2,9 +2,12 @@ __author__ = 'kaushik'
 
 import datetime, time
 
+#Display time
 print(datetime.datetime.fromtimestamp(time.time()))
 
+#get user input
 dti = input("DDMMYYYY ?")
 
+#convert user input to date.
 dt = datetime.datetime.strptime(dti,"%d%m%Y")
 print (dt)
