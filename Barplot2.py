e3ca9c55ab7b25f0410c8ad0fd769499b0463ef0
Kaushik Nayak <kaushikjnayak@gmail.com>
2017-07-04 20:42:05 +0530

    Bar, Barh
     matplotlib.pyplot.bar(left, height, width=0.8, bottom=None, hold=None, data=None, **kwargs)
        Makes a bar plot.
    
     matplotlib.pyplot.barh(bottom, width, height=0.8, left=None, hold=None, **kwargs)
        Make a horizontal bar plot.
    
    Parameters:	
    
    bottom : scalar or array-like
    
        the y coordinate(s) of the bars
    
    width : scalar or array-like
    
        the width(s) of the bars
    
    height : sequence of scalars, optional, default: 0.8
    
        the heights of the bars
    
    left : sequence of scalars

--- Barplot2.py
+++ Barplot2.py
@@ -16,6 +16,7 @@ women_pop = np.array([5., 30., 45., 22.])
 men_pop     = np.array( [5., 25., 50., 20.])
 X = np.arange(4)
 
+#Horizontal Bar
 plt.barh(X, women_pop, color = 'r')
 plt.barh(X, -men_pop, color = 'b')
 plt.show()
